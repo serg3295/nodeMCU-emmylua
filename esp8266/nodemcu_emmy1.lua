b0fba481979363e442d0e843b3e1e17072f72c4d
serg3295 <serg3295@gmail.com>
2020-12-17 10:56:47 +0300

    Add bloom

--- esp8266/nodemcu_emmy1.lua
+++ esp8266/nodemcu_emmy1.lua
@@ -640,8 +640,37 @@ function bit.rshift(value, shift) end
 ---@return integer
 function bit.set(value, pos1, ...) end
 
---*** BLOOM TODO ***
+--*** BLOOM ***
 bloom = {}
+
+---@class bloom
+local filter = bloom.create()
+
+---Create a filter object.
+---@param elements number elements The largest number of elements to be added to the filter.
+---@param errorrate number errorrate The error rate (the false positive rate).
+---@return any objfilter A filter object.
+function bloom.create(elements, errorrate) end
+
+---Adds a string to the set and returns an indication of whether the string was already present.
+---@param str string string The string to be added to the filter set.
+---@return boolean b true if the string was already present in the filter. false otherwise.
+function filter:add(str) end
+
+---Checks to see if a string is present in the filter set.
+---@param str string string The string to be checked for membership in the set.
+---@return boolean b true if the string was already present in the filter. false otherwise.
+function filter:check(str) end
+
+---Empties the filter.
+function filter:reset() end
+
+---Get some status information on the filter.
+---@return number bits The number of bits in the filter.
+---@return number fns The number of hash functions in use.
+---@return number occupancy The number of bits set in the filter.
+---@return number fprate The approximate chance that the next check will return true when it should return false
+function filter:info() end
 
 --*** BME280 ***
 bme280 = {}
