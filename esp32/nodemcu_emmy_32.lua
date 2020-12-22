e705e263425d7a02d4d5008c97c0a19d5fb293e0
serg3295 <serg3295@gmail.com>
2020-12-22 21:46:30 +0300

    Update module time

--- esp32/nodemcu_emmy_32.lua
+++ esp32/nodemcu_emmy_32.lua
@@ -1540,30 +1540,63 @@ time = {}
 
 ---Converts calendar table to a timestamp in Unix epoch
 ---@param calendar table
----@return integer
+-- `calendar` Table containing calendar info.
+--- `year` 1970 ~ 2038
+--- `mon` month 1 ~ 12 in current year
+--- `day` day 1 ~ 31 in current month
+--- `hour`
+--- `min`
+--- `sec`
+---@return integer n number of seconds since the Epoch
 function time.cal2epoch(calendar) end
 
 ---Converts timestamp in Unix epoch to calendar format
----@param time integer
+---@param time integer number of seconds since the Epoch
 ---@return table
+-- A *table* containing the fields:
+--- `year` 1970 ~ 2038
+--- `mon` month 1 ~ 12 in current year
+--- `day` day 1 ~ 31 in current month
+--- `hour`
+--- `min`
+--- `sec`
+--- `yday` day 1 ~ 366 in current year
+--- `wday` day 1 ~ 7 in current weak (Sunday is 1)
+--- `dst` day time adjustment:
+--- 1 (DST in effect, i.e. daylight time)
+--- 0 (DST not in effect, i.e. standard time)
+--- -1 (Unknown DST status)
 function time.epoch2cal(time) end
 
 ---Returns current system time in the Unix epoch (seconds from midnight 1970/01/01).
----@return integer sec
----@return integer usec
+---@return integer sec seconds since the Unix epoch
+---@return integer usec the microseconds part
 function time.get() end
 
 ---Returns current system time adjusted for the locale in calendar format.
 ---@return table
+-- A *table* containing the fields:
+--- `year` 1970 ~ 2038
+--- `mon` month 1 ~ 12 in current year
+--- `day` day 1 ~ 31 in current month
+--- `hour`
+--- `min`
+--- `sec`
+--- `yday` day 1 ~ 366 in current year
+--- `wday` day 1 ~ 7 in current weak (Sunday is 1)
+--- `dst` day time adjustment:
+-- 1 (DST in effect, i.e. daylight time)
+-- 0 (DST not in effect, i.e. standard time)
+-- -1 (Unknown DST status)
 function time.getlocal() end
 
 ---Initializes and starts NTP client
----@param ntpAddr string | '""'
+---@param ntpAddr? string address of a NTP server, defaults to "pool.ntp.org" if none is specified
 ---@return nil
 function time.initntp(ntpAddr) end
 
 ---Checks if NTP client is enabled.
----@return boolean
+---@return boolean b `true' if NTP client is enabled.
 function time.ntpenabled() end
 
 ---Stops NTP client.
@@ -1571,12 +1604,12 @@ function time.ntpenabled() end
 function time.ntpstop() end
 
 ---Sets system time to a given timestamp in the Unix epoch (seconds from midnight 1970/01/01).
----@param time integer
+---@param time integer number of seconds since the Epoch
 ---@return nil
 function time.set(time) end
 
 ---Sets correct format for Time Zone locale
----@param timezone string|'"EST+3"'
+---@param timezone string a string representing timezone, can also include DST adjustment.
 ---@return nil
 function time.settimezone(timezone) end
 
