7159596ad236106ff56e6362233c350ee80ede5e
serg3295 <serg3295@gmail.com>
2021-02-08 17:22:02 +0300

    Support ftp, mcp23017 functions

--- plugin.lua
+++ plugin.lua
@@ -1,32 +1,28 @@
 ---@param  uri  string # The uri of file
 ---@param  text string # The content of file
----@return nil|diff[]
+---@return diff[]
 function OnSetText(uri, text)
   local diffs = {}
 
-  for displTypePos, finish in text:gmatch '[%w_]+%s*%=%s*ucg%.()[%w_]+_hw_spi()' do
-    diffs[#diffs+1] = {
-        start  = displTypePos,
-        finish = finish - 1,
-        text   = ('ucgDisplayType'),
-    }
+  local subst = {
+    ['[%w_]+%s*%=%s*ucg%.()[%w_]+_hw_spi()']         = 'ucgDisplayType',
+    ['[%w_]+%s*%=%s*u8g2%.()[%w]+_i2c_[%w_]+()']     = 'u8g2DisplayTypeI2C',
+    ['[%w_]+%s*%=%s*u8g2%.()[%w]+_[^i][%w_]+()']     = 'u8g2DisplayTypeSPI',
+    ['()require[%(%s\"\']+ftpserver[%)\"\']+()']     = 'FTP',
+    ['()require[%(%s\"\']+liquidcrystal[%)\"\']+()'] = 'lc_meta',
+    ['()require[%(%s\"\']+mcp23017[%)\"\']+()']      = 'mcp23017',
+  }
+
+  for patt, _ in pairs(subst) do
+    for startPos, finishPos in text:gmatch(patt) do
+      diffs[#diffs+1] = {
+        start  = startPos,
+        finish = finishPos - 1,
+        text   = ('%s'):format(subst[patt]),
+      }
+    end
   end
 
-  for displTypePos, finish in text:gmatch '[%w_]+%s*%=%s*u8g2%.()[%w]+_i2c_[%w_]+()' do
-    diffs[#diffs+1] = {
-        start  = displTypePos,
-        finish = finish - 1,
-        text   = ('u8g2DisplayTypeI2C'),
-    }
-  end
+  return diffs
 
-  for displTypePos, finish in text:gmatch '[%w_]+%s*%=%s*u8g2%.()[%w]+_[^i][%w_]+()' do
-    diffs[#diffs+1] = {
-        start  = displTypePos,
-        finish = finish - 1,
-        text   = ('u8g2DisplayTypeSPI'),
-    }
-  end
-
-   return diffs
 end
