63581aa51d3f72ff8b52e712887d19c667307df2
serg3295 <serg3295@gmail.com>
2021-02-10 09:02:21 +0300

    Add type mcp23017

--- plugin.lua
+++ plugin.lua
@@ -11,16 +11,25 @@ function OnSetText(uri, text)
     ['()require[%(%s\"\']+ftpserver[%)\"\']+()']     = 'FTP',
     ['()require[%(%s\"\']+liquidcrystal[%)\"\']+()'] = 'lc_meta',
     ['()require[%(%s\"\']+mcp23017[%)\"\']+()']      = 'mcp23017',
+    ['()require[%(%s\"\']+bme280[%)\"\']+()']        = 'bme280',
   }
 
-  for patt, _ in pairs(subst) do
+  for patt, txt in pairs(subst) do
     for startPos, finishPos in text:gmatch(patt) do
       diffs[#diffs+1] = {
         start  = startPos,
         finish = finishPos - 1,
-        text   = ('%s'):format(subst[patt]),
+        text   = ('%s'):format(txt),
       }
     end
+  end
+
+  for localPos in text:gmatch '[^%-]%s()local%s+[%w_]+%s*=%s*mcp23017%s*%(' do
+    diffs[#diffs+1] = {
+      start  = localPos,
+      finish = localPos - 1,
+      text   = ('---@type mcp23017\n')
+    }
   end
 
   return diffs
