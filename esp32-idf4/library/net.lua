ca6feafba56568e39fb91b529fb18eb251f0f892
serg3295 <serg3295@gmail.com>
2022-12-01 18:04:22 +0300

    Update `net` module

--- esp32-idf4/library/net.lua
+++ esp32-idf4/library/net.lua
@@ -59,8 +59,8 @@ function net.multicastLeave(if_ip, multicast_ip) end
 function netsrv:close() end
 
 ---Listen on port from IP address.
----@overload fun(self: netsrv, callback: function): nil
----@overload fun(self: netsrv, port: integer, callback: function): nil
+---@overload fun(self: netsrv, callback: fun(net.socket:netsocket)): nil
+---@overload fun(self: netsrv, port: integer, callback: fun(net.socket:netsocket)): nil
 ---@param port? integer @(optional) number, can be omitted (random port will be chosen)
 ---@param ip? string @(optional) IP address string, can be omitted
 ---@param callback fun(net.socket:netsocket) @"`function(net.socket)`, pass to caller function as param if  \n a connection is created successfully"
