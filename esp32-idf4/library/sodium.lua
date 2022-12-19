ab70e2e12f6da1de16d658b114576bcb486b2076
serg3295 <serg3295@gmail.com>
2022-12-19 11:20:42 +0300

    Update `sodium`

--- esp32-idf4/library/sodium.lua
+++ esp32-idf4/library/sodium.lua
@@ -11,20 +11,21 @@ sodium.random = {}
 ---@class sodium.crypto_box
 sodium.crypto_box = {}
 
----Returns a random integer between\
----`0` and `0xFFFFFFFF` inclusive.
----@return integer @"A uniformly-distributed random  \n integer between 0 and 0xFFFFFFFF inclusive."
+---Returns a random signed 32-bit integer between `INT32_MIN` and `INT32_MAX`. On builds with 64-bit integer support (`5.3-int64-xxx`), you can convert this to an unsigned 32-bit integer with `result % (1 << 32)`, or on builds with only 64-bit double support (`5.1-doublefp`, `5.3-int32-doublefp`), with `result % (2 ^ 32)`.\
+---If WiFi is not started by calling `wifi.start()` before calling this function, then the result will only be pseudo-random, not truly random.
+---@return integer @A uniformly-distributed random 32-bit integer.
 ---@nodiscard
 function sodium.random.random() end
 
----Returns a random integer 0 <= result < upper_bound
----@param upper_bound integer @must be an integer *<= 0xFFFFFFFF*.
+---Returns a random integer `0 <= result < upper_bound`. Unlike `sodium.random.random() % upper_bound`, it guarantees a uniform distribution of the possible output values even when `upper_bound` is not a power of 2. Specifying an `upper_bound > 0x7FFFFFFF` is not recommended because the behavior will vary depending on the Lua build configuration.\
+---If WiFi is not started by calling `wifi.start()` before calling this function, then the result will only be pseudo-random, not truly random.
+---@param upper_bound integer @an integer.
 ---@return integer @An integer *>= 0* and *< upper_bound*
 ---@nodiscard
 function sodium.random.uniform(upper_bound) end
 
----Generates *n* bytes of random data. Wifi must be started,\
----by calling `wifi.start()`, before calling this function.
+---Generates `n` bytes of random data. If WiFi is not started by calling `wifi.start()` before\
+---calling this function, then the result will only be pseudo-random, not truly random.
 ---@param n number @number of bytes to return.
 ---@return string @A string of *n* random bytes.
 ---@nodiscard
