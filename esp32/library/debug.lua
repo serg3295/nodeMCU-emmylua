23aa80ce5e049ff2d9f857b9efeb5163357e732a
serg3295 <serg3295@gmail.com>
2023-03-03 16:27:46 +0300

    Add debug.getstring function

---
+++ esp32/library/debug.lua
@@ -0,0 +1,13 @@
+---@meta
+
+--=== debug ===
+
+---@class debug
+debug = {}
+
+---Returns a sorted array of the strings returned from\
+---the `lua_getstrings` function.
+---@param type string|'RAM'|'ROM'
+---@return table
+---@nodiscard
+function debug.getstrings(type) end
