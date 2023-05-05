b281d4340e242287dd7ff26443148479ac8a9a94
serg3295 <serg3295@gmail.com>
2023-05-05 17:37:31 +0300

    Update net, pipe, tmr modules

--- library/net.lua
+++ library/net.lua
@@ -10,19 +10,19 @@ net = {}
 ---@class net.dns
 net.dns = {}
 
----@class netsocket
-local netsocket = {}
+---@class socket
+local socket = {}
 
----@class netsrv
-local netsrv = {}
+---@class server
+local server = {}
 
 ---@class udpsocket
 local udpsocket = {}
 
 ---Creates a client.
----@param type? integer|`net.TCP` @"(optional) `net.TCP` (default) or `net.UDP` There`s no such thing as  \n a UDP connection because UDP is connection*less*. Thus no connection `type` parameter  \n should be required. For UDP use `net.createUDPSocket()` instead."
+---@param type? integer|`net.TCP` @"(optional) `net.TCP` (default) or `net.UDP` There\`s no such thing as  \n a UDP connection because UDP is connectionless. Thus no connection `type` parameter  \n should be required. For UDP use `net.createUDPSocket()` instead."
 ---@param secure? integer @"(optional) 0 for plain (default). Secure connections are not supported,  \n and the `secure` parameter is ignored."
----@return netsocket @>
+---@return socket @>
 --- - for `net.TCP` - net.socket sub module
 --- - for `net.UDP` - net.udpsocket sub module
 ---@nodiscard
@@ -31,7 +31,7 @@ function net.createConnection(type, secure) end
 ---Creates a server.
 ---@param type? integer|`net.TCP` @"(optional) The `type` parameter will be removed in upcoming  \n releases so that `net.createServer` will always create a TCP-based server.  \n For UDP use `net.createUDPSocket()` instead."
 ---@param timeout? integer @"(optional) for a TCP server timeout is 1~28'800 seconds,  \n 30 sec by default (for an inactive client to be disconnected)"
----@return netsrv @>
+---@return server @>
 --- - for `net.TCP` - net.server sub module
 --- - for `net.UDP` - net.udpsocket sub module
 ---@nodiscard
@@ -56,78 +56,78 @@ function net.multicastLeave(if_ip, multicast_ip) end
 
 ---Closes the server.
 ---@return nil
-function netsrv:close() end
+function server:close() end
 
 ---Listen on port from IP address.
----@overload fun(self: netsrv, callback: fun(net.socket:netsocket)): nil
----@overload fun(self: netsrv, port: integer, callback: fun(net.socket:netsocket)): nil
+---@overload fun(self: server, callback: fun(netsocket: socket)): nil
+---@overload fun(self: server, port: integer, callback: fun(netsocket: socket)): nil
 ---@param port? integer @(optional) number, can be omitted (random port will be chosen)
 ---@param ip? string @(optional) IP address string, can be omitted
----@param callback fun(net.socket:netsocket) @"`function(net.socket)`, pass to caller function as param if  \n a connection is created successfully"
+---@param callback fun(netsocket: socket) @"`function(net.socket)`, pass to caller function as param if  \n a connection is created successfully"
 ---@return nil
-function netsrv:listen(port, ip, callback) end
+function server:listen(port, ip, callback) end
 
 ---Returns server local address/port.
 ---@return integer|nil @port or `nil` if not listening
 ---@return string|nil @ip or `nil` if not listening
 ---@nodiscard
-function netsrv:getaddr() end
+function server:getaddr() end
 
 ---Closes socket.
 ---@return nil
-function netsocket:close() end
+function socket:close() end
 
 ---Connect to a remote server.
 ---@param port integer @port number
 ---@param ip_or_domain string @IP address or domain name string
 ---@return nil
-function netsocket:connect(port, ip_or_domain) end
+function socket:connect(port, ip_or_domain) end
 
 ---Provides DNS resolution for a hostname.
 ---@param domain string @domain name
----@param callback fun(net.socket:netsocket, ip:string) @`function(net.socket, ip)`.
+---@param callback fun(netsocket: socket, ip: string) @`function(net.socket, ip)`.
 ---The first parameter is the socket, the second parameter is the IP address as a string.
 ---@return nil
-function netsocket:dns(domain, callback) end
+function socket:dns(domain, callback) end
 
 ---Retrieve port and ip of remote peer.
 ---@return integer|nil @port or `nil` if not connected
 ---@return string|nil @ip or `nil` if not connected
 ---@nodiscard
-function netsocket:getpeer() end
+function socket:getpeer() end
 
 ---Retrieve local port and ip of socket.
 ---@return integer|nil @port or `nil` if not connected
 ---@return string|nil @ip or `nil` if not connected
 ---@nodiscard
-function netsocket:getaddr() end
+function socket:getaddr() end
 
 ---@deprecated
 ---This call is no longer available, as the underlying functionality was removed upstream.
 ---@return nil
-function netsocket:hold() end
+function socket:hold() end
 
 ---Register callback functions for specific events.
 ---@param event string|"connection"|"reconnection"|"disconnection"|"receive"|"sent" @event
----@param callback fun(net.socket:netsocket, str:string) @`function(net.socket[, string])`. Can be `nil` to remove callback.
+---@param callback fun(netsocket: socket, data_or_error: string|number)|nil @`function(net.socket[, string])`. Can be `nil` to remove callback.
 ---The first parameter of callback is the socket.
 --- - If event is `"receive"`, the second parameter is the received data as string.
 --- - If event is `"disconnection"` or `"reconnection"`, the second parameter is error code.\
 --If `"reconnection"` event is specified, disconnection receives only "normal close" events.\
 --Otherwise, all connection errors (with normal close) passed to disconnection event.
 ---@return nil
-function netsocket:on(event, callback) end
+function socket:on(event, callback) end
 
 ---Sends data to remote peer.
----@param str string @data in string which will be sent to server
----@param callback? fun(sent:string) @(optional) `function(sent)` for sending string
+---@param data string @data in string which will be sent to server
+---@param callback? fun(sent: socket) @(optional) `function(sent)` for sending string
 ---@return nil
-function netsocket:send(str, callback) end
+function socket:send(data, callback) end
 
 ---@deprecated
 ---This call is no longer available, as the underlying functionality was removed upstream.
 ---@return nil
-function netsocket:unhold() end
+function socket:unhold() end
 
 ---Closes UDP socket.
 ---@return nil
@@ -141,7 +141,7 @@ function udpsocket:listen(port, ip) end
 
 ---Register callback functions for specific events.
 ---@param event string|"receive"|"sent"|"dns" @receive | sent | dns
----@param callback fun(net.socket:netsocket, str:string) @`function(net.socket[, string])`. Can be `nil` to remove callback.
+---@param callback fun(netsocket: udpsocket, str?: string)|nil @`function(net.udpsocket[, string])`. Can be `nil` to remove callback.
 ---The first parameter of callback is the socket.
 --- - If event is `"receive"`, the second parameter is the received data as string.\
 ---The `receive` callback receives port and ip *after* the data argument.\
@@ -158,7 +158,7 @@ function udpsocket:send(port, ip, data) end
 
 ---Provides DNS resolution for a hostname.
 ---@param domain string @domain name
----@param callback fun(net.socket:netsocket, ip:string) @`function(net.socket, ip)`.
+---@param callback fun(netsocket: udpsocket, ip: string) @`function(net.udpsocket, ip)`.
 ---The first parameter is the socket, the second parameter is the IP address as a string.
 ---@return nil
 function udpsocket:dns(domain, callback) end
@@ -177,7 +177,7 @@ function net.dns.getdnsserver(dns_index) end
 
 ---Resolve a hostname to an IP address. Doesn't require a socket like `net.socket.dns()`.
 ---@param host string @hostname to resolve
----@param callback fun(ip:string) @`function(ip)` called when the name was resolved.
+---@param callback fun(ip: string) @`function(ip)` called when the name was resolved.
 ---@return nil
 function net.dns.resolve(host, callback) end
 
--- library/tmr.lua
+++ library/tmr.lua
@@ -8,11 +8,11 @@
 ---@field ALARM_SEMI integer
 tmr = {}
 
----@class tObj
-local tObj = {}
+---@class tmrObj
+local tmrObj = {}
 
 ---Creates a dynamic timer object.
----@return tObj @timer object
+---@return tmrObj @timer object
 ---@nodiscard
 function tmr.create() end
 
@@ -24,42 +24,42 @@ function tmr.create() end
 ---This is a convenience function combining `tmr.obj:register()` and `tmr.obj:start()` into a single call.
 ---@param interval integer @timer interval in milliseconds. Maximum value is 6870947 (1:54:30.947).
 ---@param mode t32 @timer mode
----@param callback fun(t:tObj) @function which is invoked with the timer object as an argument
+---@param callback fun(t:tmrObj) @function which is invoked with the timer object as an argument
 ---@return boolean
-function tObj:alarm(interval, mode, callback) end
+function tmrObj:alarm(interval, mode, callback) end
 
 ---Changes a registered timer's expiry interval.
 ---@param interval integer @"new timer interval in milliseconds.  \n Maximum value is 6870947 (1:54:30.947)."
 ---@return nil
-function tObj:interval(interval) end
+function tmrObj:interval(interval) end
 
 ---Configures a timer and registers the callback function to call on expiry.
 ---@param interval integer @timer interval in milliseconds. Maximum value is 6870947 (1:54:30.947).
 ---@param mode t32 @timer mode
----@param callback fun(t:tObj) @function which is invoked with the timer object as an argument
+---@param callback fun(t:tmrObj) @function which is invoked with the timer object as an argument
 ---@return nil
-function tObj:register(interval, mode, callback) end
+function tmrObj:register(interval, mode, callback) end
 
 ---Starts or restarts a previously configured timer.
 ---@return boolean
-function tObj:start() end
+function tmrObj:start() end
 
 ---Checks the state of a timer.
 ---@return boolean|nil @If the specified timer is registered, returns whether it is currently started
 ---@return integer|nil @and its mode. If the timer is not registered, `nil` is returned.
 ---@nodiscard
-function tObj:state() end
+function tmrObj:state() end
 
 ---Stops a running timer, but does not unregister it.\
 ---A stopped timer can be restarted with `tmr.obj:start()`.
 ---@return boolean
-function tObj:stop() end
+function tmrObj:stop() end
 
 ---Stops the timer (if running) and unregisters the associated callback.\
 ---This isn't necessary for one-shot timers (*tmr.ALARM_SINGLE*),\
 ---as those automatically unregister themselves when fired.
 ---@return nil
-function tObj:unregister() end
+function tmrObj:unregister() end
 
 ---Resets the watchdog timer to prevent a reboot due to a perceived hung task.\
 ---Use with caution, as this could prevent a reboot to recover from a genuinely hung task.
