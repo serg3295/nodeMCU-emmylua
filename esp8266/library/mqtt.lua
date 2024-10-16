c51ad63dc6aa640fd213deeb583930e781dfbd91
serg3295 <serg3295@gmail.com>
2024-10-16 16:48:33 +0300

    upd(8266) mqtt, wifi

--- esp8266/library/mqtt.lua
+++ esp8266/library/mqtt.lua
@@ -16,8 +16,17 @@
 ---@field CONNACK_REFUSED_NOT_AUTHORIZED integer
 mqtt = {}
 
----@class MQTT
-local MQTT = {}
+---@class mqttObj
+---Registers a callback function for an event.
+---@field on fun(self: mqttObj, event: "connect", callback: fun(client: mqttObj))
+---@field on fun(self: mqttObj, event: "connfail", callback: fun(client: mqttObj, connectionFailureCode: integer)))
+---@field on fun(self: mqttObj, event: "suback", callback: fun(client: mqttObj))
+---@field on fun(self: mqttObj, event: "unsuback", callback: fun(client: mqttObj))
+---@field on fun(self: mqttObj, event: "puback", callback: fun(client: mqttObj))
+---@field on fun(self: mqttObj, event: "message", callback: fun(client: mqttObj, topic: string, message: string))
+---@field on fun(self: mqttObj, event: "overflow", callback: fun(client: mqttObj, topic :string, message: string))
+---@field on fun(self: mqttObj, event: "offline", callback: fun(client: mqttObj))
+local mqttObj = {}
 
 ---Creates a MQTT client.
 ---@param clientid string @client ID
@@ -26,22 +35,22 @@ local MQTT = {}
 ---@param password? string @(optional) user password
 ---@param cleansession? integer @(optional) **0/1** for `false/true`. Default is 1 (`true`).
 ---@param max_message_length? integer @(optional) how large messages to accept. Default is 1024.
----@return MQTT @MQTT client
+---@return mqttObj @MQTT client
 ---@nodiscard
 function mqtt.Client(clientid, keepalive, username, password, cleansession, max_message_length) end
 
 ---Closes connection to the broker.
 ---@return nil
-function MQTT:close() end
+function mqttObj:close() end
 
 ---Connects to the broker specified by the given host, port, and secure options.
 ---@param host string @host, domain or IP
 ---@param port? integer|`1883` @(optional) broker port (number), default 1883
 ---@param secure? boolean @(optional) if `true`, use TLS.
----@param conn_est? fun(client: MQTT) @(optional) `function(client)` callback function for when the connection was established
----@param conn_not_est? fun(client: MQTT, reason:mqtt) @"(optional) `function(client, reason)` callback function for when the connection could not be established.  \n No further callbacks should be called."
+---@param conn_est? fun(client: mqttObj) @(optional) `function(client)` callback function for when the connection was established
+---@param conn_not_est? fun(client: mqttObj, reason: mqtt) @"(optional) `function(client, reason)` callback function for when the connection could not be established.  \n No further callbacks should be called."
 ---@return nil
-function MQTT:connect(host, port, secure, conn_est, conn_not_est) end
+function mqttObj:connect(host, port, secure, conn_est, conn_not_est) end
 
 ---Setup [Last Will and Testament](http://www.hivemq.com/blog/mqtt-essentials-part-9-last-will-and-testament).
 ---@param topic string @the topic to publish to (string)
@@ -49,37 +58,32 @@ function MQTT:connect(host, port, secure, conn_est, conn_not_est) end
 ---@param qos? integer|`0`|`1`|`2` @(optional) QoS level, default 0
 ---@param retain? integer|`0`|`1` @(optional) retain flag, default 0
 ---@return nil @`nil`; use callbacks to observe the outcome.
-function MQTT:lwt(topic, message, qos, retain) end
+function mqttObj:lwt(topic, message, qos, retain) end
 
 ---Registers a callback function for an event.
----@param event string|"connect"|"connfail"|"suback"|"unsuback"|"puback"|"message"|"overflow"|"offline" @event
----@param callback fun(client:MQTT, topic:string, message:string) @"`function(client[, topic[, message]])`. The first parameter is always the client object itself.  \n Any remaining parameters passed differ by event:"
---- - If event is `"message"`, the 2nd and 3rd parameters are received topic and message, respectively, as Lua strings.
---- - If the event is `"overflow"`, the parameters are as with `"message"``,\
----save that the message string is truncated to the maximum message size.
---- - If the event is `"connfail"`, the 2nd parameter will be the connection failure code.
---- - Other event types do not provide additional arguments.
+---@param event string
+---@param callback function
 ---@return nil
-function MQTT:on(event, callback) end
+function mqttObj:on(event, callback) end
 
 ---Publishes a message.
 ---@param topic string @the topic to publish to (topic string)
 ---@param payload string @the message to publish, (buffer or string)
 ---@param qos integer|`0`|`1`|`2` @QoS level
 ---@param retain integer|`0`|`1` @retain flag
----@param callback? fun(client:MQTT) @(optional) `function(client)` fired when PUBACK received (for QoS 1 or 2) or when message sent (for QoS 0).
+---@param callback? fun(client: mqttObj) @(optional) `function(client)` fired when PUBACK received (for QoS 1 or 2) or when message sent (for QoS 0).
 ---@return boolean
-function MQTT:publish(topic, payload, qos, retain, callback) end
+function mqttObj:publish(topic, payload, qos, retain, callback) end
 
 ---Subscribes to one or several topics.
 ---@param topic string|table @a topic string, or table array of `topic, qos` pairs to subscribe to.
 ---@param qos integer|`0`|`1`|`2` @QoS subscription level, default 0
----@param callback? fun(client:MQTT) @(optional) `function(client)` fired when subscription(s) succeeded.
+---@param callback? fun(client: mqttObj) @(optional) `function(client)` fired when subscription(s) succeeded.
 ---@return boolean
-function MQTT:subscribe(topic, qos, callback) end
+function mqttObj:subscribe(topic, qos, callback) end
 
 ---Unsubscribes from one or several topics.
 ---@param topic string|table @a topic string, or table array of `topic, anything` pairs to unsubscribe from.
----@param callback? fun(client:MQTT) @(optional) `function(client)` fired when unsubscription(s) succeeded.
+---@param callback? fun(client: mqttObj) @(optional) `function(client)` fired when unsubscription(s) succeeded.
 ---@return boolean
-function MQTT:unsubscribe(topic, callback) end
+function mqttObj:unsubscribe(topic, callback) end
