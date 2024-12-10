b7beecc84c561562117e647cf388f7ec997961e4
serg3295 <serg3295@gmail.com>
2024-12-10 12:25:16 +0300

    upd(8266) gpio, tmr modules

--- esp8266/library/gpio.lua
+++ esp8266/library/gpio.lua
@@ -52,8 +52,8 @@ function gpio.serout(pin, start_level, delay_times, cycle_num, callback) end
 ---|"both" #both edges
 ---|"low" #low level
 ---|"high" #high level
----|"none" #the interrupt is disabled
----@param callback? fun(level:number, when:number, eventcount:number):nil @(optional) `function(level, when, eventcount)` when trigger occurs. The parameters are:
+---|"none" #the callback function is removed and the interrupt is disabled
+---@param callback? fun(level:integer, when:integer, eventcount:integer):any @(optional) `function(level, when, eventcount)` when trigger occurs. The parameters are:
 --- - level - The level of the specified pin at the interrupt
 --- - when - The timestamp of the event. This is in microseconds and has the same base as for **tmr.now()**.
 --- - eventcount - is the number of interrupts that were elided for this callback.
